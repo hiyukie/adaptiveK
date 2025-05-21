ede721dd11764c6e374bfe3d4a24667d75073035
hiyukie <yukiekiekie.yao@gmail.com>
2025-05-21 08:12:19 +0000

    demo_config.py

---
+++ demo_config.py
@@ -0,0 +1,406 @@
+from dataclasses import dataclass, asdict, field
+from typing import Optional, Type, Any
+from enum import Enum
+import torch as t
+import itertools
+
+from dictionary_learning.trainers.standard import StandardTrainer, StandardTrainerAprilUpdate
+from dictionary_learning.trainers.top_k import TopKTrainer, AutoEncoderTopK
+from dictionary_learning.trainers.batch_top_k import BatchTopKTrainer, BatchTopKSAE
+from dictionary_learning.trainers.gdm import GatedSAETrainer
+from dictionary_learning.trainers.p_anneal import PAnnealTrainer
+from dictionary_learning.trainers.jumprelu import JumpReluTrainer
+from dictionary_learning.trainers.dynamic_topk import DynamicTopKTrainer, AutoEncoderDynamicTopK
+from dictionary_learning.trainers.matryoshka_batch_top_k import (
+    MatryoshkaBatchTopKTrainer,
+    MatryoshkaBatchTopKSAE,
+)
+from dictionary_learning.dictionary import (
+    AutoEncoder,
+    GatedAutoEncoder,
+    AutoEncoderNew,
+    JumpReluAutoEncoder,
+)
+from dictionary_learning.trainers.dynamic_topk import AutoEncoderDynamicTopK 
+
+class TrainerType(Enum):
+    STANDARD = "standard"
+    STANDARD_NEW = "standard_new"
+    TOP_K = "top_k"
+    BATCH_TOP_K = "batch_top_k"
+    GATED = "gated"
+    P_ANNEAL = "p_anneal"
+    JUMP_RELU = "jump_relu"
+    Matryoshka_BATCH_TOP_K = "matryoshka_batch_top_k"
+    DYNAMIC_TOP_K = "dynamic_top_k"
+
+@dataclass
+class LLMConfig:
+    llm_batch_size: int
+    context_length: int
+    sae_batch_size: int
+    dtype: t.dtype
+
+
+@dataclass
+class SparsityPenalties:
+    standard: list[float]
+    standard_new: list[float]
+    p_anneal: list[float]
+    gated: list[float]
+
+# dynamic SAE
+num_tokens = 256_000_000
+# baseline SAEs
+# num_tokens = 250_000
+
+print(f"NOTE: Training on {num_tokens} tokens")
+
+eval_num_inputs = 10000
+random_seeds = [0]
+dictionary_widths = [2**14]
+
+WARMUP_STEPS = 10
+SPARSITY_WARMUP_STEPS = 15
+DECAY_START_FRACTION = 0.8
+learning_rates = [1e-3]
+
+wandb_project = "pythia-160m-sweep"
+
+LLM_CONFIG = {
+    "EleutherAI/pythia-70m-deduped": LLMConfig(
+        llm_batch_size=64, context_length=1024, sae_batch_size=2048, dtype=t.float32
+    ),
+    "EleutherAI/pythia-160m-deduped": LLMConfig(
+        llm_batch_size=32, context_length=1024, sae_batch_size=2048, dtype=t.float32
+    ),
+    "google/gemma-2-2b": LLMConfig(
+        llm_batch_size=4, context_length=1024, sae_batch_size=2048, dtype=t.bfloat16
+    ),
+}
+
+# SPARSITY_PENALTIES = SparsityPenalties(
+#     standard=[2.4, 3.6, 4.8, 8, 12, 16],
+#     standard_new=[2.4, 3.6, 4.8, 8, 12, 16],
+#     p_anneal=[1.2, 1.8, 2.4, 4, 6, 8],
+#     gated=[2.4, 3.6, 4.8, 8, 12, 16],
+# )
+
+SPARSITY_PENALTIES = SparsityPenalties(
+    standard=[0.6, 0.9, 1.2, 2, 3, 4],
+    standard_new=[0.6, 0.9, 1.2, 2, 3, 4],
+    p_anneal=[0.3, 0.45, 0.6, 1, 1.5, 2],
+    gated=[0.6, 0.9, 1.2, 2, 3, 4],
+)
+
+TARGET_L0s = [20, 40, 80, 160, 320, 640]
+
+@dataclass
+class BaseTrainerConfig:
+    activation_dim: int
+    device: str
+    layer: str
+    lm_name: str
+    submodule_name: str
+    trainer: Type[Any]
+    dict_class: Type[Any]
+    wandb_name: str
+    warmup_steps: int
+    steps: int
+    decay_start: Optional[int]
+
+@dataclass
+class StandardTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    l1_penalty: float
+    sparsity_warmup_steps: Optional[int]
+    resample_steps: Optional[int] = None
+
+@dataclass
+class StandardNewTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    l1_penalty: float
+    sparsity_warmup_steps: Optional[int]
+
+@dataclass
+class PAnnealTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    initial_sparsity_penalty: float
+    sparsity_warmup_steps: Optional[int]
+    sparsity_function: str = "Lp^p"
+    p_start: float = 1.0
+    p_end: float = 0.3
+    anneal_start: int = 50
+    anneal_end: Optional[int] = None
+    sparsity_queue_length: int = 5
+    n_sparsity_updates: int = 5
+
+@dataclass
+class TopKTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    k: int
+    auxk_alpha: float = 1 / 32
+    threshold_beta: float = 0.999
+    threshold_start_step: int = 5
+
+@dataclass
+class MatryoshkaBatchTopKTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    k: int
+    group_fractions: list[float] = field(
+        default_factory=lambda: [
+            (1 / 32),
+            (1 / 16),
+            (1 / 8),
+            (1 / 4),
+            ((1 / 2) + (1 / 32)),
+        ]
+    )
+    group_weights: Optional[list[float]] = None
+    auxk_alpha: float = 1 / 32
+    threshold_beta: float = 0.999
+    threshold_start_step: int = 20
+
+@dataclass
+class GatedTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    l1_penalty: float
+    sparsity_warmup_steps: Optional[int]
+
+@dataclass
+class JumpReluTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    lr: float
+    target_l0: int
+    sparsity_warmup_steps: Optional[int]
+    sparsity_penalty: float = 1.0
+    bandwidth: float = 0.001
+
+@dataclass
+class DynamicTopKTrainerConfig(BaseTrainerConfig):
+    dict_size: int
+    seed: int
+    sae_lr: float
+    probe_lr: float
+    l1_weight: float
+    probe_weight: float
+    base_k: int
+    min_k: int
+    max_k: int
+    auxk_alpha: float = 1/32
+    scores_path: Optional[str] = None
+
+def get_trainer_configs(
+    architectures: list[str],
+    learning_rates: list[float],
+    seeds: list[int],
+    activation_dim: int,
+    dict_sizes: list[int],
+    model_name: str,
+    device: str,
+    layer: str,
+    submodule_name: str,
+    steps: int,
+    warmup_steps: int = WARMUP_STEPS,
+    sparsity_warmup_steps: int = SPARSITY_WARMUP_STEPS,
+    decay_start_fraction=DECAY_START_FRACTION,
+) -> list[dict]:
+    decay_start = int(steps * decay_start_fraction)
+
+    trainer_configs = []
+
+    base_config = {
+        "activation_dim": activation_dim,
+        "steps": steps,
+        "warmup_steps": warmup_steps,
+        "decay_start": decay_start,
+        "device": device,
+        "layer": layer,
+        "lm_name": model_name,
+        "submodule_name": submodule_name,
+    }
+    if TrainerType.P_ANNEAL.value in architectures:
+        for seed, dict_size, learning_rate, sparsity_penalty in itertools.product(
+            seeds, dict_sizes, learning_rates, SPARSITY_PENALTIES.p_anneal
+        ):
+            config = PAnnealTrainerConfig(
+                **base_config,
+                trainer=PAnnealTrainer,
+                dict_class=AutoEncoder,
+                sparsity_warmup_steps=sparsity_warmup_steps,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                initial_sparsity_penalty=sparsity_penalty,
+                wandb_name=f"PAnnealTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.STANDARD.value in architectures:
+        for seed, dict_size, learning_rate, l1_penalty in itertools.product(
+            seeds, dict_sizes, learning_rates, SPARSITY_PENALTIES.standard
+        ):
+            config = StandardTrainerConfig(
+                **base_config,
+                trainer=StandardTrainer,
+                dict_class=AutoEncoder,
+                sparsity_warmup_steps=sparsity_warmup_steps,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                l1_penalty=l1_penalty,
+                wandb_name=f"StandardTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.STANDARD_NEW.value in architectures:
+        for seed, dict_size, learning_rate, l1_penalty in itertools.product(
+            seeds, dict_sizes, learning_rates, SPARSITY_PENALTIES.standard_new
+        ):
+            config = StandardNewTrainerConfig(
+                **base_config,
+                trainer=StandardTrainerAprilUpdate,
+                dict_class=AutoEncoder,
+                sparsity_warmup_steps=sparsity_warmup_steps,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                l1_penalty=l1_penalty,
+                wandb_name=f"StandardTrainerNew-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.GATED.value in architectures:
+        for seed, dict_size, learning_rate, l1_penalty in itertools.product(
+            seeds, dict_sizes, learning_rates, SPARSITY_PENALTIES.gated
+        ):
+            config = GatedTrainerConfig(
+                **base_config,
+                trainer=GatedSAETrainer,
+                dict_class=GatedAutoEncoder,
+                sparsity_warmup_steps=sparsity_warmup_steps,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                l1_penalty=l1_penalty,
+                wandb_name=f"GatedTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.TOP_K.value in architectures:
+        for seed, dict_size, learning_rate, k in itertools.product(
+            seeds, dict_sizes, learning_rates, TARGET_L0s
+        ):
+            config = TopKTrainerConfig(
+                **base_config,
+                trainer=TopKTrainer,
+                dict_class=AutoEncoderTopK,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                k=k,
+                wandb_name=f"TopKTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.BATCH_TOP_K.value in architectures:
+        for seed, dict_size, learning_rate, k in itertools.product(
+            seeds, dict_sizes, learning_rates, TARGET_L0s
+        ):
+            config = TopKTrainerConfig(
+                **base_config,
+                trainer=BatchTopKTrainer,
+                dict_class=BatchTopKSAE,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                k=k,
+                wandb_name=f"BatchTopKTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.Matryoshka_BATCH_TOP_K.value in architectures:
+        for seed, dict_size, learning_rate, k in itertools.product(
+            seeds, dict_sizes, learning_rates, TARGET_L0s
+        ):
+            config = MatryoshkaBatchTopKTrainerConfig(
+                **base_config,
+                trainer=MatryoshkaBatchTopKTrainer,
+                dict_class=MatryoshkaBatchTopKSAE,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                k=k,
+                wandb_name=f"MatryoshkaBatchTopKTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.JUMP_RELU.value in architectures:
+        for seed, dict_size, learning_rate, target_l0 in itertools.product(
+            seeds, dict_sizes, learning_rates, TARGET_L0s
+        ):
+            config = JumpReluTrainerConfig(
+                **base_config,
+                trainer=JumpReluTrainer,
+                dict_class=JumpReluAutoEncoder,
+                sparsity_warmup_steps=sparsity_warmup_steps,
+                lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                target_l0=target_l0,
+                wandb_name=f"JumpReluTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+
+    if TrainerType.DYNAMIC_TOP_K.value in architectures:
+
+        context_count = 250000
+        desired_epochs = 15
+        batch_size = 2048
+        dynamic_steps = (context_count * desired_epochs) // batch_size
+
+        dynamic_decay_start = int(dynamic_steps * DECAY_START_FRACTION)
+        dynamic_warmup_steps = 15
+        # dynamic_sparsity_warmup_steps = 250
+
+        dynamic_base_config = dict(base_config)
+        dynamic_base_config["steps"] = dynamic_steps
+        dynamic_base_config["decay_start"] = dynamic_decay_start
+        dynamic_base_config["warmup_steps"] = dynamic_warmup_steps
+        # dynamic_base_config["sparsity_warmup_steps"] = dynamic_sparsity_warmup_steps
+
+        for seed, dict_size, learning_rate in itertools.product(
+            seeds, dict_sizes, learning_rates
+        ): 
+            config = DynamicTopKTrainerConfig(
+                **dynamic_base_config,
+                trainer=DynamicTopKTrainer,
+                dict_class=AutoEncoderDynamicTopK,
+                sae_lr=learning_rate,
+                probe_lr=learning_rate,
+                dict_size=dict_size,
+                seed=seed,
+                l1_weight=0.005,
+                probe_weight=0.9,
+                base_k=80,
+                min_k=20, 
+                max_k=320,
+                auxk_alpha=1/32,
+                wandb_name=f"DynamicTopKTrainer-{model_name}-{submodule_name}",
+            )
+            trainer_configs.append(asdict(config))
+    
+    return trainer_configs
